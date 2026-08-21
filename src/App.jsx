acfd0d5577e742cf606f04cb6fc1240468c51168
sofia dario <sofiadcdario03@gmail.com>
2026-08-21 18:58:53 +0800

    password checker fully done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./Navbar";
 import Activity1 from "./Activity1";
+import Activity3 from "./Activity3";
 
 function Home() {
   return (
@@ -32,6 +33,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/activity1" element={<Activity1 />} />
+          <Route path="/activity3" element={<Activity3 />} />
         </Routes>
 
       </div>
