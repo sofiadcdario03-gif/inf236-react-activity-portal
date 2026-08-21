1af9a1cda9927e6d043dd2e7e0f23c90aee126f8
sofia dario <sofiadcdario03@gmail.com>
2026-08-21 22:42:38 +0800

    Resolve merge conflicts in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ import Navbar from "./Navbar";
 import Activity1 from "./Activity1";
 import GradeEvaluation from "./GradeEvaluation";
 import Activity3 from "./Activity3";
-<<<<<<< HEAD
-import AttendanceChecker from "./AttendanceChecker";
-=======
 import Activity5 from "./Activity5";
->>>>>>> 19c7f777dfe6f9abf9ae0809ab881a67b30a8ef9
 
 function Home() {
   const activities = [
@@ -98,31 +94,9 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-
-          <Route
-            path="/activity1"
-            element={<Activity1 />}
-          />
-
-          <Route
-            path="/activity2"
-            element={<GradeEvaluation />}
-          />
-
-          <Route
-            path="/activity3"
-            element={<Activity3 />}
-          />
-
-          {/* Activity 4 and 5 routes can be added
-              once you send their components. */}
-
-=======
           <Route path="/activity1" element={<Activity1 />} />
           <Route path="/activity3" element={<Activity3 />} />
           <Route path="/activity5" element={<Activity5 />} />
->>>>>>> 19c7f777dfe6f9abf9ae0809ab881a67b30a8ef9
         </Routes>
 
       </div>
